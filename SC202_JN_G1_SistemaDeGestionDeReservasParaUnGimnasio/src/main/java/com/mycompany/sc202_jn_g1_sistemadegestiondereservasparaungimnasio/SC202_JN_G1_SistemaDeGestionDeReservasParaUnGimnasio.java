213781e2847132176d3a3dd0fe060cfafa347732
SaTsSo <Samuel.Tsai.Solis@gmail.com>
2025-07-19 16:15:49 -0600

    Creación del archivo base

---
+++ SC202_JN_G1_SistemaDeGestionDeReservasParaUnGimnasio/src/main/java/com/mycompany/sc202_jn_g1_sistemadegestiondereservasparaungimnasio/SC202_JN_G1_SistemaDeGestionDeReservasParaUnGimnasio.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sc202_jn_g1_sistemadegestiondereservasparaungimnasio;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author samim
+ */
+public class SC202_JN_G1_SistemaDeGestionDeReservasParaUnGimnasio {
+
+    public static void main(String[] args) {
+        
+              
+    }
+}
