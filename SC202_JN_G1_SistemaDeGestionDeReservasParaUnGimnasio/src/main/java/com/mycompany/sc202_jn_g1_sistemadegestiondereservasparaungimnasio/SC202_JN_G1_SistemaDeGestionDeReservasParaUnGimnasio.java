ff2d2f4a96ad6001047ef3aed8fa8c9dfe4fcc64
SaTsSo <Samuel.Tsai.Solis@gmail.com>
2025-08-08 18:09:50 -0600

    Programacion de la funcionalidad validarCredenciales

--- SC202_JN_G1_SistemaDeGestionDeReservasParaUnGimnasio/src/main/java/com/mycompany/sc202_jn_g1_sistemadegestiondereservasparaungimnasio/SC202_JN_G1_SistemaDeGestionDeReservasParaUnGimnasio.java
+++ SC202_JN_G1_SistemaDeGestionDeReservasParaUnGimnasio/src/main/java/com/mycompany/sc202_jn_g1_sistemadegestiondereservasparaungimnasio/SC202_JN_G1_SistemaDeGestionDeReservasParaUnGimnasio.java
@@ -16,25 +16,22 @@ public class SC202_JN_G1_SistemaDeGestionDeReservasParaUnGimnasio {
         
         Usuario [] usuarios = new Usuario [10];
         
-        usuarios [0] = new Usuario (111111111, "samuel", 83.1, 19999999,"samuel@prueba.com", "tsai", 22);
-        usuarios [1] = new Usuario (222222222, "matias", 74.2, 28888888,"matias@prueba.com", "Cabalceta", 18);
-        usuarios [2] = new Usuario (333333333, "santiago", 65.3, 37777777,"santiago@prueba.com", "paniagua", 19);
-        usuarios [3] = new Usuario (444444444, "jimena", 60.4, 466666666,"jimena@prueba.com", "lopez", 20);
-        usuarios [4] = new Usuario (1, "vacio", 1.1, 1,"vacio@prueba.com", "vacio", 1);
-        usuarios [5] = new Usuario (1, "vacio", 1.1, 1,"vacio@prueba.com", "vacio", 1);
-        usuarios [6] = new Usuario (1, "vacio", 1.1, 1,"vacio@prueba.com", "vacio", 1);
-        usuarios [7] = new Usuario (1, "vacio", 1.1, 1,"vacio@prueba.com", "vacio", 1);
-        usuarios [8] = new Usuario (1, "vacio", 1.1, 1,"vacio@prueba.com", "vacio", 1);
-        usuarios [9] = new Usuario (1, "vacio", 1.1, 1,"vacio@prueba.com", "vacio", 1);
+        usuarios [0] = new Usuario ("111111111", "samuel", 83.1, "19999999","samuel@prueba.com", "tsai", 22);
+        usuarios [1] = new Usuario ("222222222", "matias", 74.2, "28888888","matias@prueba.com", "Cabalceta", 18);
+        usuarios [2] = new Usuario ("333333333", "santiago", 65.3, "37777777","santiago@prueba.com", "paniagua", 19);
+        usuarios [3] = new Usuario ("444444444", "jimena", 60.4, "466666666","jimena@prueba.com", "lopez", 20);
+        usuarios [4] = new Usuario ("1", "vacio", 1.1, "1","vacio@prueba.com", "vacio", 1);
+        usuarios [5] = new Usuario ("1", "vacio", 1.1, "1","vacio@prueba.com", "vacio", 1);
+        usuarios [6] = new Usuario ("1", "vacio", 1.1, "1","vacio@prueba.com", "vacio", 1);
+        usuarios [7] = new Usuario ("1", "vacio", 1.1, "1","vacio@prueba.com", "vacio", 1);
+        usuarios [8] = new Usuario ("1", "vacio", 1.1, "1","vacio@prueba.com", "vacio", 1);
+        usuarios [9] = new Usuario ("1", "vacio", 1.1, "1","vacio@prueba.com", "vacio", 1);
         
         boolean salir = false;
-
+        
         //Inicio de sesion
-        String Usuario = JOptionPane.showInputDialog("Ingrese su correo de usuario: ");
-        String Contraseña = JOptionPane.showInputDialog("Ingrese su contraseña: ");
-        //VALIDAR LAS CREDENCIALES
-        
-        
+        ValidarInicioSesion validarInicioSesion = new ValidarInicioSesion(usuarios);
+        validarInicioSesion.validarCredenciales();
 
         do {
             //Menu principal
--- SC202_JN_G1_SistemaDeGestionDeReservasParaUnGimnasio/src/main/java/com/mycompany/sc202_jn_g1_sistemadegestiondereservasparaungimnasio/ValidarInicioSesion.java
+++ SC202_JN_G1_SistemaDeGestionDeReservasParaUnGimnasio/src/main/java/com/mycompany/sc202_jn_g1_sistemadegestiondereservasparaungimnasio/ValidarInicioSesion.java
@@ -4,24 +4,48 @@
  */
 package com.mycompany.sc202_jn_g1_sistemadegestiondereservasparaungimnasio;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author samim
  */
 public class ValidarInicioSesion {
     
+    private Usuario[] usuariosRegistrados;
     
-    public boolean validarCredenciales (String usuario, String contrasena){
-        
-        
-        
+    public ValidarInicioSesion (Usuario[] usuariosRegistrados){
+        this.usuariosRegistrados = usuariosRegistrados;
+    }
+    
+    public boolean validarCredenciales (){
         boolean correcto = false;
+        int intento = 0;
         
-        if (usuario == ){
+        do{
+            String correo = JOptionPane.showInputDialog("Ingrese su correo de usuario: ");
+            String contrasena = JOptionPane.showInputDialog("Ingrese su contraseña: ");
+            intento += 1;
             
-        }
-        
-        return correcto;
+            for (int i = 0; i < usuariosRegistrados.length; i++) {
+                if (usuariosRegistrados[i].getCorreo().equals(correo) && usuariosRegistrados[i].getContrasena().equals(contrasena)) {
+                    correcto = true;
+                    JOptionPane.showMessageDialog(null, "Inicio de sesion correcto!\nVienvenido " + usuariosRegistrados[i].getNombre());
+                }
+            }
+            
+            if (correcto == false){
+                if (intento < 3){
+                    JOptionPane.showMessageDialog(null, "Las credenciales ingresado son invalidas, intentelo de nuevo");
+                }else if (intento == 3){
+                    JOptionPane.showMessageDialog(null, "Ha ingresado 3 veces credenciales incorrectas\nSaliendo...");
+                    System.exit(0);
+                }
+            }
+            
+            
+            return correcto;
+        } while (intento == 3);
     }
     
 }
